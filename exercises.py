7a2cdc458deb4f047475acc159e92c1c295e7132
Florian Diegruber <florian@diegruber.net>
2018-10-26 14:43:10 +0200

    create linear search function

---
+++ exercises.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 14:35:25 2018
+
+@author: flo
+"""
+
+#%%
+
+def linear(lst, elem):
+    for i in range(len(lst)):
+        if elem == lst[i]:
+            return i
+    
+    return None
+#%%
